4be954d0ee180be77b27a88e51df734de93bb7a4
kanoop40 <kanoop2547@gmail.com>
2025-05-20 07:34:42 +0700

    new

--- frontend/src/api.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-const API_URL = "http://192.168.2.33:3000/api";// ถ้า backend รันบน server จริง เปลี่ยน URL นี้
-
-export const register = (data) => axios.post(`${API_URL}/users/register`, data);
-export const login = (data) => axios.post(`${API_URL}/users/login`, data);
