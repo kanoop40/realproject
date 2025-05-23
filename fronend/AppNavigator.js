23d7ccfad20694c507e2c4c6e9aabf94fbd7d0aa
kanoop40 <kanoop2547@gmail.com>
2025-05-23 17:39:06 +0700

    s

--- fronend/AppNavigator.js
+++ fronend/AppNavigator.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from '../screens/LoginScreen';
-import ChatScreen from '../screens/ChatScreen';
+import ChatScreen from './screens/ChatScreen';
 
 const Stack = createStackNavigator();
 
--- fronend/socket.js
+++ fronend/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io('http://<YOUR_BACKEND_IP>:5000', {
+const socket = io("http://192.168.1.34:5000/api", {
   transports: ['websocket'],
   // auth: { token: "..." }
 });
