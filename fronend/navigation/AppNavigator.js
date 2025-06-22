4149f34a4731f68351c5c7ca5b2c7bc225404801
kanoop40 <kanoop2547@gmail.com>
2025-06-23 03:15:22 +0700

    sd

--- fronend/navigation/AppNavigator.js
+++ fronend/navigation/AppNavigator.js
@@ -3,12 +3,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createSharedElementStackNavigator } from 'react-navigation-shared-element'; // Use Shared Element Navigator
 import WelcomeScreen from '../screens/WelcomeScreen';
 import LoginScreen from '../screens/LoginScreen';
-import RegisterScreen from '../screens/RegisterScreen';
-import ChatScreen from '../screens/ChatScreen';
-import ProfileScreen from '../screens/ProfileScreen';
-import CreateGroupScreen from '../screens/CreateGroupScreen';
-import SearchUserScreen from '../screens/SearchUserScreen.js';
-import ChatRoomScreen from '../screens/ChatRoomScreen';
 
 const Stack = createSharedElementStackNavigator();
 
@@ -41,39 +35,7 @@ const AppNavigator = () => (
           return [{ id: 'sharedLogo', animation: 'fade' }]; // Shared element transition for logo
         }}
       />
-      <Stack.Screen
-        name="Register"
-        component={RegisterScreen}
-        options={{
-          title: 'สมัครสมาชิก',
-          headerShown: true,
-        }}
-      />
-      <Stack.Screen
-        name="Chat"
-        component={ChatScreen}
-        options={{ title: 'แชท' }}
-      />
-      <Stack.Screen
-        name="Profile"
-        component={ProfileScreen}
-        options={{ headerShown: false }}
-      />
-      <Stack.Screen
-        name="CreateGroup"
-        component={CreateGroupScreen}
-        options={{ title: 'สร้างกลุ่ม' }}
-      />
-      <Stack.Screen
-        name="SearchUser"
-        component={SearchUserScreen}
-        options={{ title: 'ค้นหาผู้ใช้' }}
-      />
-      <Stack.Screen
-        name="ChatRoom"
-        component={ChatRoomScreen}
-        options={{ title: 'ห้องแชท' }}
-      />
+      
     </Stack.Navigator>
   </NavigationContainer>
 );
