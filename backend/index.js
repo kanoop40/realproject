38d7c7cd179da8f415fe1e86ff745752006f3d2e
kanoop40 <kanoop2547@gmail.com>
2025-06-23 11:24:18 +0700

    s

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
-const chatRoutes = require('./routes/chatRoutes');
-const fileRoutes = require('./routes/fileRoutes');
-const notificationRoutes = require('./routes/notificationRoutes');
-const classroomRoutes = require('./routes/classroomRoutes');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
-const path = require('path');
 
 // Load env vars
 dotenv.config();
@@ -20,26 +15,15 @@ const app = express();
 // Body parser
 app.use(express.json());
 
-// Static folder
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
 // Mount routes
 app.use('/api/users', userRoutes);
-app.use('/api/chat', chatRoutes);
-app.use('/api/files', fileRoutes);
-app.use('/api/notifications', notificationRoutes);
-app.use('/api/classrooms', classroomRoutes);
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 // Error handlers
 app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-// Setup Socket.io
-const setupSocket = require('./socket/socket');
-setupSocket(server);
